397422f4e3d8dbe60b6dd54b432f8b8182074928
Md Anas Sabah <mdanassabah@gmail.com>
2022-08-23 21:50:12 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 import useBookSearch from "./useBookSearch";
 
 function App() {
   const [query, setQuery] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
+  const { book, hasMore, loading, error } = useBookSearch(query, pageNumber);
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   function handleSearch(e) {
     setQuery(e.target.value);
     setPageNumber(1);
   }
 
-  const { book, hasMore, loading, error } = useBookSearch(query, pageNumber);
   return (
     <>
       <input type="text" value={query} onChange={handleSearch} />
-      {book.map((b) => {
-        return <div key={b}>{b}</div>;
+      {book.map((b, index) => {
+        if (book.length === index + 1) {
+          return (
+            <div ref={lastBookElementRef} key={b}>
+              {b}
+            </div>
+          );
+        } else {
+          return <div key={b}>{b}</div>;
+        }
       })}
       <div> {loading && "Loading ..."} </div>
       <div> {error && "Error..."} </div>
